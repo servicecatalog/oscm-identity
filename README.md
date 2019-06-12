f242ec62f9e818c510877d83b18330207c862e7c
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-06-12 09:41:19 +0200

    Initail project structure and travis configuration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # oscm-identity
+
+[![Build status](https://travis-ci.org/servicecatalog/oscm-identity.svg?branch=master)](https://travis-ci.org/servicecatalog/oscm-identity)
 Identity service for managing authentication with external IdPs using OpenId Connect
---
+++ src/main/java/org/oscm/OSCMIdentityApplication.java
@@ -0,0 +1,12 @@
+package org.oscm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OSCMIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OSCMIdentityApplication.class, args);
+    }
+}
