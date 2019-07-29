59c30529836960523d34cf52ad5594991a1f7a56
adamczykba <bartlomiej.adamczyk1@ts.fujitsu.com>
2019-07-29 12:53:23 +0200

    Modified tenant property retrieval logic to work according to current context

--- src/main/java/org/oscm/identity/oidc/tenant/ClasspathTenantPropertyRetriever.java
+++ src/main/java/org/oscm/identity/oidc/tenant/ClasspathTenantPropertyRetriever.java
@@ -3,24 +3,43 @@
  *
  * <p>Copyright FUJITSU LIMITED 2019
  *
- * <p>Creation Date: July 22, 2019
+ * <p>Creation Date: July 29, 2019
  *
  * <p>*****************************************************************************
  */
 package org.oscm.identity.oidc.tenant;
 
+import org.oscm.identity.error.TenantConfigurationException;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
-/**
- * Retrieves tenant properties from the classpath
- */
+/** Retrieves tenant properties from the classpath */
 @Component
+@Profile({"test", "dev"})
 public class ClasspathTenantPropertyRetriever implements TenantPropertyFileRetriever {
+
+  private Properties properties;
 
   @Override
   public Properties retrievePropertiesFor(String tenantId) {
-    return null;
+    properties = new Properties();
+
+    try {
+      File file = new ClassPathResource("tenant-" + tenantId + ".properties").getFile();
+      InputStream inputStream = new FileInputStream(file);
+      properties.load(inputStream);
+    } catch (IOException exc) {
+      throw new TenantConfigurationException(
+          "Tenant configuration [tenantId = " + tenantId + "] could not be loaded", exc);
+    }
+
+    return properties;
   }
 }
--- src/main/java/org/oscm/identity/oidc/tenant/FilesystemTenantPropertyRetriever.java
+++ src/main/java/org/oscm/identity/oidc/tenant/FilesystemTenantPropertyRetriever.java
@@ -1,4 +1,67 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2019
+ *
+ * <p>Creation Date: July 29, 2019
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.identity.oidc.tenant;
 
-public class FilesystemTenantPropertyRetriever implements  {
+import lombok.extern.slf4j.Slf4j;
+import org.oscm.identity.error.TenantConfigurationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.Properties;
+
+/** Retrieves tenant properties from directory specified in <i>application.properties</i> */
+@Slf4j
+@Component
+@Profile("prod")
+public class FilesystemTenantPropertyRetriever implements TenantPropertyFileRetriever {
+
+  @Value("${tenant.config.directory.relative.path}")
+  private String tenantConfigDirectoryRelativePath;
+
+  private Properties properties;
+
+  @Override
+  public Properties retrievePropertiesFor(String tenantId) {
+    properties = new Properties();
+    String propertyFilePath = getPropertiesFilePathForTenant(tenantId);
+
+    try {
+      InputStream inputStream = getFileInputSteam(propertyFilePath);
+      properties.load(inputStream);
+    } catch (IOException exc) {
+      TenantConfigurationException ex =
+          new TenantConfigurationException(
+              "Tenant configuration [tenantId = " + tenantId + "] could not be loaded", exc);
+
+      log.error(ex.getMessage(), exc);
+      throw ex;
+    }
+
+    return properties;
+  }
+
+  private String getPropertiesFilePathForTenant(String tenantId) {
+    return getTenantConfigDirectoryPath() + "tenant-" + tenantId + ".properties";
+  }
+
+  private String getTenantConfigDirectoryPath() {
+    File classPath = new File(System.getProperty("java.class.path"));
+    String jarAbsolutePath = classPath.getAbsolutePath();
+
+    return jarAbsolutePath.substring(0, jarAbsolutePath.length() - classPath.getName().length())
+        + tenantConfigDirectoryRelativePath;
+  }
+
+  protected InputStream getFileInputSteam(String propertyFilePath) throws FileNotFoundException {
+    return new FileInputStream(propertyFilePath);
+  }
 }
