d9f70a3203230e91063dd50b82084fa155806cf1
OSCM Automaton <oscm.automaton@gmail.com>
2021-11-26 13:35:10 +0000

    Applied code formatting [ci skip]

--- src/main/java/org/oscm/identity/error/IdTokenValidationException.java
+++ src/main/java/org/oscm/identity/error/IdTokenValidationException.java
@@ -1,29 +1,34 @@
+/**
+ * *****************************************************************************
+ *
+ * <p>Copyright FUJITSU LIMITED 2021
+ *
+ * <p>Creation Date: 26-11-2021
+ *
+ * <p>*****************************************************************************
+ */
 package org.oscm.identity.error;
 
-/**
- * Exception that is thrown, when validation of the ID Token fails
- */
+/** Exception that is thrown, when validation of the ID Token fails */
 public class IdTokenValidationException extends TokenValidationException {
 
-        
-        public IdTokenValidationException(String message) {
-                super(message);
-        }
+  public IdTokenValidationException(String message) {
+    super(message);
+  }
 
-        public IdTokenValidationException(String message, String errorCode) {
-                super(message, errorCode);
-        }
+  public IdTokenValidationException(String message, String errorCode) {
+    super(message, errorCode);
+  }
 
-        public IdTokenValidationException(Throwable exception) {
-                super(exception);
-        }
+  public IdTokenValidationException(Throwable exception) {
+    super(exception);
+  }
 
-        public IdTokenValidationException(String message, Throwable exception) {
-                super(message, exception);
-        }
+  public IdTokenValidationException(String message, Throwable exception) {
+    super(message, exception);
+  }
 
-        public IdTokenValidationException(String message, String errorCode,
-                Throwable exception) {
-                super(message, errorCode, exception);
-        }
+  public IdTokenValidationException(String message, String errorCode, Throwable exception) {
+    super(message, errorCode, exception);
+  }
 }
